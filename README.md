11cc121b0628ea1ba16a5ecf2704d5d5509d359d
Zdenek Kubala <kubala@ygg.cz>
2016-11-13 10:59:40 +0100

    added general folder + priprav_projekt modified README added gitignore

--- README.md
+++ README.md
@@ -2,6 +2,20 @@ automation_stuff
 ================
 Things which help me managing servers or with something else.
 
+general dir
+-----------
+Things which do multiple things
+* priprav_projekt.sh - prepare things for an new project
+
+monitor
+-------
+Set of scripts for monitoring the system.
+* diskusage_monitor.sh - disk usage script
+* monitor.sh - monitor file - overal system  watch
+* ps_mem.py - process memory consumption
+* swap.sh - swap usage by process
+* performace_tools - list of performace tools
+
 mysql dir
 ---------
 Some plans to improve dump&restore.
@@ -10,11 +24,3 @@ Some plans to improve dump&restore.
 * remote_backup_mysql.sh - remote copy script
 * mysql_tuner.pl- mysql_tuner - tune mysql
 
-monitor
--------
-Set of scripts for monitoring the system.
-* disk usage script
-* monitor file - overal system  watch
-* ps_mem - process memory consumption
-* swap - swap usage by process
-* performace tools
---
+++ general/priprav_projekt.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#---------------------------------------
+#### START      HEADER
+## script for add user to the system and create directory by Zdenek K <n1djz.88@ygg.cz>
+## usage ./priprav_projekt.sh username
+## debug option
+##set -x
+## TO DO
+## automate more stuff
+#### END        HEADER
+#---------------------------------------
+
+#### START      FUNCTIONS DECLARATION
+#### END        FUNCTION DECLARATION
+
+#### START      BASIC VARIABLES
+NEW_UID=$((`tail -1 /etc/passwd | awk -F \: '{print $3}'` + 1));
+USER_UID=$NEW_UID;
+USERNAME="$1";
+HESLO=`apg`
+WRONGUSERID="10000"
+#### END        BASIC VARIABLES
+
+#### START      ARGUMENT PARSING
+
+# variable validation
+if [ -z $1 ];then echo "usage: $0 user";exit 1;fi
+if (( NEW_UID < $WRONGUSERID ));then echo "check passwd file, userid is lower than $WRONGUSERID";exit 1;fi
+
+# Sumary of inputs before creating
+echo -e "\nSummary: \nuser: \"${USERNAME}\", UID:\"$USER_UID\""
+read -p "Is it correct [y/n]? " Y_N
+case $Y_N in
+	[yY]) ;;
+           *) echo "correct inputs..."; exit 1;;
+esac
+echo
+#### END	ARGUMENT PARSING
+
+#### START      MAIN 
+
+# creation of user
+useradd -d /var/www/html/${USERNAME} --no-create-home --user-group --uid $USER_UID --shell /bin/false --inactive 0 --password '$HESLO' --comment "ftp uzivatel ${USERNAME}" ${USERNAME} && echo "heslo: $HESLO"
+
+# creation of a dir
+mkdir -p /var/www/html/${USERNAME}/http && chmod -R g+s /var/www/html/${USERNAME} && chown -R ${USERNAME}:projektyftp /var/www/html/${USERNAME} && chmod u-w /var/www/html/${USERNAME} && chmod g+w /var/www/html/${USERNAME}/http
+
+# have to do actions
+echo -e  "Do manually:\n ----------------"
+echo "ADD user to vsftp allow user file AND reload configuration"
+echo "CP apache site template AND reload configuration"
+echo "Create database and add dbname to the mysql-dump file"
+
+#### END	MAIN 
